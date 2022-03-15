d9cd23c16cefdd9752af8b8d41814596d9221b80
bmisarts <bmisarts@gmail.com>
2022-03-15 20:45:14 +0100

    gestion des users

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid bg-primary p-5 hero-header mb-5">
         <div class="row py-5">
             <div class="col-12 text-center">
-                <h1 class="display-1 text-white animated zoomIn">Articles du Blog</h1>
+                <h1 class="display-1 text-white animated zoomIn">Blog</h1>
                 <a href="{{ url('/') }}" class="h4 text-white">Accueil</a>
                 <i class="far fa-circle text-white px-2"></i>
                 <a href="{{ url('/blog') }}" class="h4 text-white">blog</a>
